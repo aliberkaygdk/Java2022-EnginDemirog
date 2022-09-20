4dd81f5032d2ba436a845568572ddd3f667914d8
Ali Berkay Gedikoğlu <berkay.gedikoglu@ogr.sakarya.edu.tr>
2022-09-20 22:12:36 +0300

    Ders8

---
+++ conditionals/src/pkg/Main.java
@@ -0,0 +1,17 @@
+package pkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//=	.
+		int sayi=24;
+		if(sayi<20) 
+			System.out.println("Sayı 20den kücük");
+		else if(sayi==20)
+			System.out.println("Sayi 20ye esit");
+		else
+			System.out.println("Sayi 20den buyuk");
+
+	}
+
+}
